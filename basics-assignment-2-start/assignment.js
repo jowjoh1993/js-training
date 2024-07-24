73d8e8f40f7b99df654283d4bae12aa36f6f8829
Joshua Johnstone <joshua.johnstone@frgrisk.com>
2024-07-24 14:42:58 -0400

    basics assignment 2

--- basics-assignment-2-start/assignment.js
+++ basics-assignment-2-start/assignment.js
@@ -1,1 +1,21 @@
 const task3Element = document.getElementById('task-3');
+
+function display() {
+   alert("whee");
+}
+
+function display2(text) {
+   alert(text);
+}
+
+display();
+display("haha");
+
+task3Element.addEventListener('click', display);
+
+function concat(text1, text2, text3) {
+   return text1 + text2 + text3;
+}
+
+alert(concat("ba", "ba", "booey"));
+
